a01807203da6e7ece1ca76813dbdebd5f7b72458
SokChanSeiha <sokchanseiha@gmail.com>
2024-11-15 00:46:08 +0700

    redendered Content

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,25 @@
-import Button from "./components/Button"
-import { DiApple, DiAndroid, DiAptana } from "react-icons/di";
+import Accordion from "./components/Accordion"
 
 function App() {
-  const onClickHandler = () => console.log("hii");
-  return (
-    <>
-      <div>
-        <Button secondary outline rounded onClick={onClickHandler} className="mb-5">
-          <DiApple/>
-          One One
-        </Button>
-      </div>
-      <div>
-        <Button danger outline>
-          <DiAndroid/>
-          Two Two
-        </Button>
-      </div>
-      <div>
-        <Button warning>
-          <DiAptana/>
-          Three
-        </Button>
-      </div>
-      <div>
-          <Button secondary outline rounded>Four</Button>
-      </div>
-      <div>
-          <Button primary rounded>Five</Button>
-      </div>
-    </>
-  )
+const items = [
+  {
+    id: "111",
+    label: "Madara",
+    content: "Nothing ever goes as planned in this accursed world. The longer you live, the more you realize that the only things that truly exist in this reality are merely pain, suffering and futility. Listen, everywhere you look in this world, wherever there is light, there will always be shadows to be found as well."
+  },
+  {
+    id: "222",
+    label: "Naruto",
+    content: "Never underestimate me! I don't quit, and I won't run! Naruto made a declaration that he would never flee from danger or abandon his duty. While this is a fairly basic sentiment, it is also an important one that became one of the most recognizable Naruto quotes."
+  },
+  {
+    id: "333",
+    label: "Tanjrio",
+    content: "Tanjiro tells Akaza: The strong protect the weak. Then the weak get stronger and help those weaker than themselves. That is the law of nature! Then, he fights him with all his power."
+  }
+]
+
+  return <Accordion items={items}/>;
 }
 
 export default App
