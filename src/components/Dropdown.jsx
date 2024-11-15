d5886268f26771f124564975cbaa26645df9b52e
SokChanSeiha <sokchanseiha@gmail.com>
2024-11-15 17:49:06 +0700

    Styling on accordion

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -15,16 +15,20 @@ function Dropdown({options, value, onChange}) {
 
     const renderedOption = options.map((option) => {
         return (
-            <div onClick={() => handleOptionClick(option)} key={option.value}>
+            <div className="hover:bg-sky-100 rounded cursor-pointer p-1" onClick={() => handleOptionClick(option)} key={option.value}>
                 {option.label}
             </div>
         )
     });
 
     return (
-        <div>
-            <div onClick={handleClick}>{value?.label || 'Selecting ...'}</div>
-            {isOpen && <div>{renderedOption}</div>}
+        <div className="w-48 relative">
+            <div className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full" onClick={handleClick}>
+                {value?.label || 'Selecting ...'}
+            </div>
+            {isOpen &&(
+                <div className="absolute top-full border rounded p-3 shadow bg-white w-full">{renderedOption}</div>
+            )}
         </div>
     )
   }
