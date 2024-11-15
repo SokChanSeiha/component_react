a1e686eb7e9b5e4ddf1ed407722e320017889d2f
SokChanSeiha <sokchanseiha@gmail.com>
2024-11-15 12:07:56 +0700

    variation event handler, eventHandler not used in rendering map, use function parameter

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -2,13 +2,15 @@ import { useState } from "react";
 
 const Accordion = ({ items }) => {
     const [expandedIndex, setExpandedIndex] = useState(0);
+    const handleClick =(nextIndex) => {
+        setExpandedIndex(nextIndex);
+    };
 
     const renderedItems = items.map((item, index) => {
         const isExpanded = index === expandedIndex;
-
         return (
           <div key={item.id}>
-              <div>{item.label}</div>
+              <div onClick={() => handleClick(index)}> {item.label} </div>
               {/* if isExpanded is false, nothing is being displayed */}
               {isExpanded && <div className="bg-red-500">{item.content}</div>} 
           </div>
