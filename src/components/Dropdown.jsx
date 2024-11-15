53aa2d5611cf456ac24616b463445ff13c5e37c3
SokChanSeiha <sokchanseiha@gmail.com>
2024-11-15 15:36:18 +0700

    implement state to show and hide the option list

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,6 +1,21 @@
+import { useState } from "react"
+import { FaStethoscope } from "react-icons/fa";
+
 function Dropdown({options}) {
+    const [isOpen, setIsOpen] = useState(false);
+    const handleClick = () => {
+        // setIsOpen((currentIsOpen) => !currentIsOpen); : totally fine
+        setIsOpen(!isOpen);
+    };
+
+    const renderedOption = options.map((option) => {
+        return <div key={option.value}> {option.label} </div>
+    });
     return (
-      <div>Dropdown</div>
+        <div>
+            <div onClick={handleClick}>Select ...</div>
+            {isOpen && <div>{renderedOption}</div>}
+        </div>
     )
   }
   
