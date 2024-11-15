55aa50a303967eb9c9252ecd788226edc90e3268
SokChanSeiha <sokchanseiha@gmail.com>
2024-11-15 11:03:53 +0700

    map the accordion to find the index of the item to be expanded

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+
 const Accordion = ({ items }) => {
-    const renderedItems = items.map((item) => {
+    const [expandedIndex, setExpandedIndex] = useState(0);
+
+    const renderedItems = items.map((item, index) => {
+        // logic to expanded within index
+        if (index === expandedIndex) {
+            console.log("expanded at", index);
+        } else {
+            console.log("collapsed at", index);
+        }
+
+        // rendered the content
+        const isExpanded = index === expandedIndex;
+
         return (
           <div key={item.id}>
               <div>{item.label}</div>
