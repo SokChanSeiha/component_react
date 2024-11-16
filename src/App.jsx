e0e71acb02137224ae39224bf3f1b5e9074bfd09
SokChanSeiha <sokchanseiha@gmail.com>
2024-11-16 14:39:58 +0700

    refactor component file, having DropdownPage and ready for navigation implemenation

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
-import Dropdown from "./components/dRopdown";
-import { useState } from "react";
-
 function App() {
-  const [selection, setSelection] = useState(null);
-  const handleSelect = (option) => {
-    setSelection(option);
-  }
-
-  const options = [
-    {label: 'Red', value: 'red'},
-    {label: 'Blue', value: 'blue'},
-    {label: 'Yellow', value: 'yellow'},
-  ];
-
   return (
-    <div className="flex">
-      <Dropdown options={options} onChange={handleSelect} value={selection} />
-      <Dropdown options={options} onChange={handleSelect} value={selection} />
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
 export default App
