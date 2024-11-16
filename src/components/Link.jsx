3b950aa2983b24753cada5c9d366e4d1289e9278
SokChanSeiha <sokchanseiha@gmail.com>
2024-11-16 15:40:11 +0700

    creating a custom navigation system in React by building a reusable Link component

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,9 +1,12 @@
 import {useContext} from 'react';
-// import NavigationContext 
+import NavigationContext from '../context/navigation';
 
 function Link({to, children}) {
+    const {navigate} = useContext(NavigationContext);
     const handleClick = (event) => {
         event.preventDefault();
+
+        navigate(to);
     };
 
     return <a onClick={handleClick}>{children}</a>
