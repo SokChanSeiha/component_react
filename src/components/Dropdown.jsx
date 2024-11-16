e0dac7133101edd787189b212a5058944b3890b9
SokChanSeiha <sokchanseiha@gmail.com>
2024-11-16 12:18:53 +0700

    useRef to get the reference of the div

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,13 +1,14 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 import Panel from "./Panel";
 import { FaChevronCircleDown } from "react-icons/fa";
 
 function Dropdown({options, value, onChange}) {
     const [isOpen, setIsOpen] = useState(false);
+    const divEl = useRef();
 
     useEffect(() => {
         const handler = (event) => {
-            console.log(event.target);
+            console.log(divEl.current); // (divEl)it doesnt give a direct reference to the div, but an object the current in it.
         };
 
         document.addEventListener('click', handler, true);
@@ -36,7 +37,7 @@ function Dropdown({options, value, onChange}) {
     });
 
     return (
-        <div className="w-48 relative">
+        <div ref={divEl} className="w-48 relative">
             <Panel className="flex justify-between items-center cursor-pointer" onClick={handleClick}>
                 {value?.label || 'Selecting ...'}
                 <FaChevronCircleDown className="text-lg"/>
