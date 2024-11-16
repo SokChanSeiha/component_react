046fa61bd870dc1a8e8da6459fe07abd856ce484
SokChanSeiha <sokchanseiha@gmail.com>
2024-11-16 16:28:14 +0700

    a bit styling on link using classnames library

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,8 +1,10 @@
+import classNames from 'classnames';
 import {useContext} from 'react';
 import NavigationContext from '../context/navigation';
 
 function Link({to, children}) {
     const {navigate} = useContext(NavigationContext);
+    const usableLinkClassNames = classNames('text-blue-500');
     const handleClick = (event) => {
         // if using one of the keys, 
         if (event.metaKey || event.ctrlKey) {
@@ -13,7 +15,7 @@ function Link({to, children}) {
         navigate(to);
     };
     // related with key commands
-    return <a href={to} onClick={handleClick}>
+    return <a  className={usableLinkClassNames} href={to} onClick={handleClick}>
         {children}</a>
 };
 
