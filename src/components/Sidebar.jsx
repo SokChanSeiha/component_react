db3919a6970e1f21f6f4b3d4e7a2f747342a9008
SokChanSeiha <sokchanseiha@gmail.com>
2024-11-16 17:08:08 +0700

    create a sidebar component and add button path into sidebar and some styling

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,22 @@
+import Link from "./Link";
+
+function Sidebar() {
+    const links = [
+        {label: "Dropdown", path: '/'},
+        {label: "Accordion", path: '/accordion'},
+        {label: "Buttons", path: '/buttons'},
+    ];
+
+    const renderedLinks = links.map((link) => {
+        // to prop is where navigation go to
+        return <Link key={link.label} to={link.path}>{link.label}</Link>;
+    });
+
+  return (
+    <div className="sticky top-0 overflow-y-scrolll flex flex-col">
+        {renderedLinks}
+    </div>
+  )
+}
+
+export default Sidebar
